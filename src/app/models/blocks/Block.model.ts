cbfb069ec61fb5ef821b653830569295523b4b9d
Unknown <alyaksyr3516@gmail.com>
2018-08-01 17:21:02 +0000

    pAbnne

--- src/app/models/blocks/Block.model.ts
+++
@@ -1,9 +0,0 @@
-export class  Block {
-  constructor(public id: number,
-              public  version: number,
-              public libelle: string,
-              public description: string,
-              public path_photo: string) {
-  }
-}
-
